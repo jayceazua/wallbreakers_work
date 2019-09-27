3fa31fb43a09c95def7a4839cc9691a45bafa1c0
Jayce Azua <jayce.azua@gmail.com>
2019-09-26 17:53:01 -0700

    solved

--- week_1/valid_para.py
+++ week_1/valid_para.py
@@ -32,8 +32,9 @@ Output: true
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
 
+        stack = []
+        # create a O(1) look up
         lookup = {
             "(": ")",
             "[": "]",
@@ -41,11 +42,13 @@ class Solution:
         }
 
         for character in s:
-
+            # if the character is an opener add it to the stack
             if character in lookup:
                 stack.append(character)
-
+            # there has to be something in the stack to compare it to and make sure it closes
+            # also, remove the first thing on the stack and see if it closes with the character given
             elif len(stack) == 0 or lookup[stack.pop()] != character:
                 return False
 
+        # if there is something still in the stack return false
         return len(stack) == 0
