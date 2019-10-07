2ada307493f9320aca85be9765ab9fa1bf844a53
Jayce Azua <jayce.azua@gmail.com>
2019-10-07 01:25:37 -0700

    solved

--- week_2/unique_morse_code_words.py
+++ week_2/unique_morse_code_words.py
@@ -29,41 +29,16 @@ words[i] will only consist of lowercase letters.
 
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        MORSE_CODE = [
-            ".-",   # a -> 0
-                    "-...",  # b -> 1
-                    "-.-.",  # c -> 2
-                    "-..",  # d -> 3
-                    ".",    # e -> 4
-                    "..-.",  # f -> 5
-                    "--.",  # g -> 6
-                    "....",  # h -> 7
-                    "..",   # i -> 8
-                    ".---",  # j -> 9
-                    "-.-",  # k -> 10
-                    ".-..",  # l -> 11
-                    "--",   # m -> 12
-                    "-.",   # n -> 13
-                    "---",  # o -> 14
-                    ".--.",  # p -> 15
-                    "--.-",  # q -> 16
-                    ".-.",  # r -> 17
-                    "...",  # s -> 18
-                    "-",    # t -> 19
-                    "..-",  # u -> 20
-                    "...-",  # v -> 21
-                    ".--",  # w -> 22
-                    "-..-",  # x -> 23
-                    "-.--",  # y -> 24
-                    "--.."  # z -> 25
-        ]
+        MORSE_CODE = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+                      "--",  "-.",  "---", ".--.", "--.-", ".-.", "...", "-",   "..-", "...-", ".--", "-..-", "-.--", "--.."]
 
-        morse = set()
-        conversion = ""
-        for word in words:
-            for letter in word:
-                conversion += MORSE_CODE[ord(letter) - ord('a')]
-            morse.add(conversion)
-            conversion = ""
+        morse = {"".join(MORSE_CODE[ord(letter) - ord('a')]
+                         for letter in word) for word in words}
+        # conversion = ""
+        # for word in words:
+        #     for letter in word:
+        #         conversion += MORSE_CODE[ord(letter) - ord('a')]
+        #     morse.add(conversion)
+        #     conversion = ""
 
         return len(morse)
