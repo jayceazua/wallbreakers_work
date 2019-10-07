1eb56ada1bf2412a0f879b4ffaded979751c6270
Jayce Azua <jayce.azua@gmail.com>
2019-10-07 14:55:02 -0700

    solved

---
+++ week_2/intersection_two_arrays.py
@@ -0,0 +1,24 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+"""
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums_1 = set(nums1)
+        nums_2 = set(nums2)
+
+        return nums_1 & nums_2
