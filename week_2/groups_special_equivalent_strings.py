72822ea6ac672c684d9d9058b8ec373249c3f299
Jayce Azua <jayce.azua@gmail.com>
2019-10-07 13:24:36 -0700

    solved

--- week_2/groups_special_equivalent_strings.py
+++ week_2/groups_special_equivalent_strings.py
@@ -49,6 +49,7 @@ class Solution:
             ans = [0] * 52
             for i, letter in enumerate(A):
                 ans[ord(letter) - ord('a') + 26 * (i % 2)] += 1
+
             return tuple(ans)
 
         return len({count(word) for word in A})
