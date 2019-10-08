4149c2893395309fac5c1bc93e016244ffd12306
Jayce Azua <jayce.azua@gmail.com>
2019-10-08 01:52:06 -0700

    solved

---
+++ week_2/isomorphic_strings.py
@@ -0,0 +1,36 @@
+"""
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+Note:
+You may assume both s and t have the same length.
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dt = {}
+        for i in range(len(s)):
+            if s[i] in dt:
+                if dt[s[i]] != t[i]:
+                    return False
+            elif t[i] in dt.values():
+                return False
+            else:
+                dt[s[i]] = t[i]
+        return True
