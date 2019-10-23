edde5c7378c439c123b46bc79c3ff5d7029f0475
Jayce Azua <jayce.azua@gmail.com>
2019-10-23 01:35:33 -0700

    solved

--- company/yelp_v2.py
+++ company/yelp_v2.py
@@ -1,9 +1,13 @@
-import operator
+from operator import attrgetter
+
 
 class Chain(object):
   def __init__(self, name, freq):
     self.name = name
     self.freq = freq
+  
+  def __repr__(self):
+    return f"{self.name} - {self.freq}"
 
 class Business(object):
   def __init__(self, name, location, _id):
@@ -14,21 +18,32 @@ class Business(object):
 
 chains = []
 businesses_objects = [
+    
     Business("Starbucks", "Seattle", 101), 
     Business("Peets Coffee", "San Francisco", 102),
     Business("Whole Foods", "Austin", 103),
     Business("Starbucks", "San Francisco", 104), 
     Business("Peets Coffee", "Austin", 105),
+    Business("Astro Burger", "Austin", 303),
+    Business("Peets Coffee", "Austin", 203),
     Business("Starbucks", "Austin", 106),
+    Business("Astro Burger", "Austin", 302),
+    Business("Starbucks", "Austin", 190),
     Business("Whole Foods", "Austin", 103),
     Business("Whole Foods", "Austin", 107),
-  ]
+    Business("Astro Burger", "Austin", 301),
+    Business("Bob's Burger", "Austin", 501),
+    Business("Bob's Burger", "Austin", 360),
+    Business("Bob's Burger", "Austin", 370),
+    Business("Bob's Burger", "Austin", 560),
 
+    
+  ]
 
 def get_unique_business_sort(businesses, location):
   seen_id = set()
   seen = {}
-
+  chains = []
   # get all unique businesses and count
   for busineess in businesses:
     if busineess.location == location:
@@ -39,8 +54,16 @@ def get_unique_business_sort(businesses, location):
           seen[busineess.name] = 1
           seen_id.add(busineess._id)
 
-  sorted_x = sorted(seen.items(), key=operator.itemgetter(1), reverse=True)
-  print(sorted_x)
+  for name, freq  in seen.items():
+    chains.append(Chain(name, freq))
+
+  # chains = sorted(chains, key=lambda chain: chain.freq, reverse=True)
+  chains = sorted(chains, key=attrgetter('freq'), reverse=True)
+
+  print(chains)
+
+
+
 
 
 get_unique_business_sort(businesses_objects, "Austin")
