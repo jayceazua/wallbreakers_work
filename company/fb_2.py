ed870012808286555238b4c7f83f70934b3e145c
Jayce Azua <jayce.azua@gmail.com>
2019-11-11 00:19:15 -0800

    solved

--- company/fb_2.py
+++ company/fb_2.py
@@ -27,61 +27,55 @@ def search(nums, target):
   # return None
 
   # Optimized Solution - finding the smallest element's index then using binary search
-  # if len(nums) == 0:
-  #     return -1
-  # # O(log n) - find the smallest element in the given array of integers
-  # smallest_index = get_rotated_index(nums, 0, len(nums) - 1)
+  if len(nums) == 0:
+      return -1
+  # O(log n) - find the smallest element in the given array of integers
+  smallest_index = get_rotated_index(nums, 0, len(nums) - 1)
 
-  # left = 0
-  # right = len(nums) - 1
+  left = 0
+  right = len(nums) - 1
 
-  # # how do we know which side to binary search on...
-  # if target >= nums[smallest_index] and target <= nums[right]:
-  #     left = smallest_index
-  # else:
-  #     right = smallest_index
+  # how do we know which side to binary search on...
+  if target >= nums[smallest_index] and target <= nums[right]:
+    # search on the right
+      left = smallest_index
+  else:
+    # otherwise search on the left
+      right = smallest_index
 
-  # # O(log n) - perform a regular binary search with an al
-  # return binary_search(nums, left, right, target)
+  # O(log n) - perform a regular binary search with an al
+  return binary_search(nums, left, right, target)
 
   # Optimized Solution - one pass binary search
-  start = 0
-  end = len(nums) - 1
-
-  while start <= end:
-      # prevents integer overflow in other languages
-      mid = start + (end - start) // 2
-
-      # found the answer
-      if nums[mid] == target:
-          return mid
-
-      elif nums[mid] >= nums[start]:
-
-          if target >= nums[start] and target < nums[mid]:
-              end = mid - 1
-          else:
-              start = mid + 1
-
-      else:
-          if target <= nums[end] and target > nums[mid]:
-              start = mid + 1
-          else:
-              end = mid - 1
-  return -1
+  # start = 0
+  # end = len(nums) - 1
+  # while start <= end:
+  #     # prevents integer overflow in other languages
+  #     mid = start + (end - start) // 2
+  #     # found the answer
+  #     if nums[mid] == target:
+  #         return mid
+  #     elif nums[mid] >= nums[start]:
+  #         if target >= nums[start] and target < nums[mid]:
+  #             end = mid - 1
+  #         else:
+  #             start = mid + 1
+  #     else:
+  #         if target <= nums[end] and target > nums[mid]:
+  #             start = mid + 1
+  #         else:
+  #             end = mid - 1
+  # return -1
 
 #regular binary search here
 def binary_search(nums, left, right, target):
   while left <= right:
       mid = left + (right - left) // 2
-
       if nums[mid] == target:
           return mid
-
-      elif nums[mid] < target:
+      if nums[mid] < target:
           # look towards the right side of the array
           left = mid + 1
-
       elif nums[mid] > target:
           # look towards the left side of the array
           right = mid - 1
@@ -93,13 +87,12 @@ def get_rotated_index(nums, left, right):
     # way of knowing if we should search left or right
   while left < right:
       mid = left + (right - left) // 2
-
       if nums[mid] > nums[right]:
+        # this tells us that the smallest will be on the right side of the array
           left = mid + 1
-
       else:
+        # otherwise we will know that the smalllest is on the left side of the array
           right = mid
-
   return left
 
 
