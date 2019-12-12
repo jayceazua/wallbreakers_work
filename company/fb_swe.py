d823facd8a1086190ad5e1621b6717f96cab044f
Jayce Azua <jayce.azua@gmail.com>
2019-12-12 13:18:44 -0800

    solved

---
+++ company/fb_swe.py
@@ -0,0 +1,91 @@
+"""Welcome to Facebook!
+
+This is just a simple shared plaintext pad, with no execution capabilities.
+
+When you know what language you would like to use for your interview,
+simply choose it from the dropdown in the top bar.
+
+Enjoy your interview!
+
+# isToeplitz
+
+# [[1]] -> true
+
+# 1 3 6 4
+# 2 1 3 6
+# 5 2 1 3 -> true
+
+# 1 3 6  4
+# 2 1 - 1 6
+# 5 2 1  3 -> false
+
+# 4 6 3 1
+# 6 3 1 2 -> false
+
+# [[1, 2, 4, 5, 6]] -> true
+
+# number of columns not equal to rows
+[
+    [1],
+    [2, 3],
+    [3, 4, 6],
+]
+"""
+
+def isToeplitz(matrix):  # O(n * m)
+
+    rows = len(matrix)  # n O(1)
+    cols = len(matrix[0])  # m
+
+    for col in range(cols):
+        if not check_diagonal(matrix, 0, col):
+            return False  # O(1)
+
+    for row in range(rows):
+        if not check_diagonal(matrix, row, 0):
+            return False
+
+    return True
+
+
+def check_diagonal(matrix, i, j):  # bool
+    constant = matrix[i][j]
+    i += 1
+    j += 1
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    while i < rows and j < cols:
+        if matrix[i][j] != constant:
+            return False
+        i += 1
+        j += 1
+
+    return True
+
+
+# "t h e w e a t h e r i s n i c e" -> "the weather is nice"
+# "t h e w e a" -> ""
+
+
+def _is_in_dictionary(substring):  # true or false
+    pass
+
+
+def turn_string_words(s):
+    result = []
+    word = ""
+
+    for _, char in enumerate(s): # O(n)
+
+        if _is_in_dictionary(word):
+            result.append(word)
+            word = ""
+
+        word += char # O(n)
+
+    if not word:
+        return ""
+
+    sentence = " ".join(result)
+    return sentence.strip()
