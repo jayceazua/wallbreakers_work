2a13e55f7a3974b9b67adac7c40b329d94c5f1ec
Jayce Azua <jayce.azua@gmail.com>
2019-12-24 14:43:29 -0500

    [Add] new method to the linked list
    Added the insert at head of linked list.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,8 +1,31 @@
+from linked_list_node import Node
+
+
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    # size of the linked list
-  
-  # inserting a new node
-  # deleting a node
-  # searching for a node
+    def __init__(self):
+        self.head = None
+        # size of the linked list
+        self.size = 0
+
+    # inserting a new node
+    def insert_head(self, data):
+        """
+          Inserting a new node at the head of the linked list is an O(1) operation.
+        """
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.set_next_node(self.head)
+            self.head = new_node
+
+        self.size += 1
+
+    def insert_tail(self, data):
+        pass
+
+    def insert_specific(self, data):
+        pass
+
+    # deleting a node
+    # searching for a node
