620ce43c3bb0d61a3b637ca21e074c7fc336f84a
Jayce Azua <jayce.azua@gmail.com>
2019-12-26 22:09:13 -0500

    queue

---
+++ data_structures/queue.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_queue(self):
+        pass
