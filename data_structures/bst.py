332892b0425cebfb505e438fc12523f782dd3e85
Jayce Azua <jayce.azua@gmail.com>
2019-12-27 01:22:00 -0500

    insert

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -1,10 +1,31 @@
 from bst_node import Node
 
+
 class BST:
     def __init__(self):
         self.root = None
 
   # insert
+  def insert(self, data):
+    def insert_node(data, current):
+      if data >= current.data:
+        # right
+        if not current.right:
+          current.right = Node(data)
+        else:
+          insert_node(data, current.right)
+      else:
+        # left
+        if not current.left:
+          current.left = Node(data)
+        else:
+          insert_node(data, current.left)
+    if not self.root:
+      self.root = Node(data)
+    else:
+      insert_node(data, self.root)
+
+
   # delete
   # search
     def find(self, data):
