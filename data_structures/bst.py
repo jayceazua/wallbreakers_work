1ccfc3f8cb8ebbcc2ce619242ab96de8be407564
Jayce Azua <jayce.azua@gmail.com>
2019-12-27 02:52:37 -0500

    bst delete case 1 and 2

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -5,7 +5,12 @@ class BST:
     def __init__(self):
         self.root = None
   # insert
+
     def insert(self, data):
+        """
+        Best case: when binary tree is a completely balanced tree
+        O(log n) runtime complexity
+        """
         def insert_node(data, current):
             if data >= current.data:
                 # right
@@ -24,7 +29,11 @@ class BST:
         else:
             insert_node(data, self.root)
   # search
+
     def find(self, data):
+        """
+        O(log n) worse case scenario
+        """
         def search(data, current):
             if data == current.data:
                 return current.data
@@ -39,6 +48,62 @@ class BST:
         return search(data, self.root)
 
   # delete
+    def delete(self, data):
+        """
+        O(log n) worse case scenario
+        Case 1: node to be deleted is a leaf
+        Case 2: node to be deleted has one child
+        Case 3: node to be deleted has two children
+        """
+        if not self.find(data):
+            return f"{data} is not in the binary search tree."
+        # get a reference of the parent node
+        current = self.root
+        parent = self.root
+        is_left_child = False
+
+        if not current:
+            return
+
+        while current and current.data != data:
+            parent = current
+            if data < current.data:
+                current = current.left
+                is_left_child = True
+            else:
+                current = current.right
+                is_left_child = False
+
+        if not current:
+            return
+
+        # Case 1
+        if not current.left and not current.right:
+            if current == self.root:
+                self.root == None
+            else:
+                if is_left_child:
+                    parent.left = None
+                else:
+                    parent.right = None
+        # Case 2
+        elif not current.right:
+            if current == self.root:
+                self.root = self.root.left
+            elif is_left_child:
+                parent.left = current.left
+            else:
+                parent.right = current.left
+
+        elif not current.left:
+            if current == self.root:
+                self.root = self.root.right
+            elif is_left_child:
+                parent.left = current.right
+            else:
+                parent.right = current.right
+        # Case 3
+
   # in-order
   # post-order
   # pre-order
