8f387370b8b96313d07274f60352e7f5579191c3
Jayce Azua <jayce.azua@gmail.com>
2019-12-27 01:23:16 -0500

    insert

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -6,32 +6,32 @@ class BST:
         self.root = None
 
   # insert
-  def insert(self, data):
-    def insert_node(data, current):
-      if data >= current.data:
-        # right
-        if not current.right:
-          current.right = Node(data)
+    def insert(self, data):
+        def insert_node(data, current):
+            if data >= current.data:
+                # right
+                if not current.right:
+                    current.right = Node(data)
+                else:
+                    insert_node(data, current.right)
+            else:
+                # left
+                if not current.left:
+                    current.left = Node(data)
+                else:
+                    insert_node(data, current.left)
+        if not self.root:
+            self.root = Node(data)
         else:
-          insert_node(data, current.right)
-      else:
-        # left
-        if not current.left:
-          current.left = Node(data)
-        else:
-          insert_node(data, current.left)
-    if not self.root:
-      self.root = Node(data)
-    else:
-      insert_node(data, self.root)
-
+            insert_node(data, self.root)
 
   # delete
   # search
+
     def find(self, data):
         def search(data, current):
             if data == current.data:
-                return root
+                return current.data
             # move left
             if current and data < current.data:
                 return search(data, current.left)
