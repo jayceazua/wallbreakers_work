1a3a22f598a2469a78a7689d09ab7b69311b9292
Jayce Azua <jayce.azua@gmail.com>
2019-12-26 22:07:18 -0500

    created my own stack...

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -2,6 +2,25 @@ class BST:
     def __init__(self):
         self.root = None
 
-    # insert
-    # delete
-    # search
+  # insert
+  # delete
+  # search
+    def find(self, data):
+        def search(data, current):
+            if data == current.data:
+                return root
+            # move left
+            if current and data < current.data:
+                return search(data, current.left)
+            # move right
+            elif current:
+                return search(data, current.right)
+            # if nothing is round
+            return None
+
+        return search(data, self.root)
+
+  # in-order
+  # post-order
+  # pre-order
+  # level-order
