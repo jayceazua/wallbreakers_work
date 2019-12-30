8866066ba5df2cf69292f8710041cf0786a90a1e
Jayce Azua <jayce.azua@gmail.com>
2019-12-30 15:35:15 -0500

    hash function

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -7,4 +7,26 @@ class HashEntry:
         self.nxt = None
 
 
+class HashTable:
+    # Constructor
+    def __init__(self):
+        # Size of the HashTable
+        self.slots = 10
+        # Current entries in the table
+        # Used while resizing the table when half of the table gets filled
+        self.size = 0
+        # List of HashEntry objects (by default all None)
+        self.bucket = [None] * self.slots
 
+    # Hash Function
+    def get_index(self, key):
+        # hash is a built in function in Python
+        hash_code = hash(key)
+        index = hash_code % self.slots
+        return index
+
+    def get_size(self):
+        return self.size
+
+    def is_empty(self):
+        return self.get_size() == 0
