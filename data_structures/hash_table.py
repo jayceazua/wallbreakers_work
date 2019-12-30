494d543c254e85981cb978d5323c9c9406498a18
Jayce Azua <jayce.azua@gmail.com>
2019-12-30 15:42:24 -0500

    finished hash table

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -55,3 +55,69 @@ class HashTable:
                 head = head.next
         self.bucket = new_bucket
         self.slots = new_slots
+
+    def insert(self, key, value):
+        # Find the node with the given key
+        b_index = self.get_index(key)
+        if self.bucket[b_index] == None:
+            self.bucket[b_index] = HashEntry(key, value)
+            print(key, "-", value, "inserted at index:", b_index)
+            self.size += 1
+        else:
+            head = self.bucket[b_index]
+            while head is not None:
+                if head.key is key:
+                    head.value = value
+                    break
+                elif head.nxt is None:
+                    head.nxt = HashEntry(key, value)
+                    print(key, "-", value, "inserted at index:", b_index)
+                    self.size += 1
+                    break
+                head = head.nxt
+
+        load_factor = float(self.size) / float(self.slots)
+        # Checks if 60% of the entries in table are filled, threshold = 0.6
+        if load_factor >= self.threshold:
+            self.resize()
+
+    def search(self, key):
+        # Find the node with the given key
+        b_index = self.get_index(key)
+        head = self.bucket[b_index]
+        # Search key in the slots
+        while head is not None:
+            if head.key is key:
+                return head.value
+            head = head.nxt
+        # If key not found
+        return None
+
+    def delete(self, key):
+        # Find index
+        b_index = self.get_index(key)
+        head = self.bucket[b_index]
+        # If key exists at first slot
+        if head.key is key:
+            self.bucket[b_index] = head.nxt
+            print(key, "-", head.value, "deleted")
+            # Decrease the size by one
+            self.size -= 1
+            return self
+        # Find the key in slots
+        prev = None
+        while head is not None:
+            # If key exists
+            if head.key is key:
+                prev.nxt = head.nxt
+                print(key, "-", head.value, "deleted")
+                # Decrease the size by one
+                self.size -= 1
+                return
+            # Else keep moving in chain
+            prev = head
+            head = head.nxt
+
+        # If key does not exist
+        print("Key not found")
+        return
