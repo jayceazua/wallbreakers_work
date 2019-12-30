58248a6ebd4f41c7e92f0c0c965eb173ef0958de
Jayce Azua <jayce.azua@gmail.com>
2019-12-30 15:29:04 -0500

    hash entry

---
+++ data_structures/hash_table.py
@@ -0,0 +1,10 @@
+class HashEntry:
+    def __init__(self, key, data):
+        self.key = key
+        # data to be stored
+        self.value = data
+        # reference to new entry
+        self.nxt = None
+
+
+
