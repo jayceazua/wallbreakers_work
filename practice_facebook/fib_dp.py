11f794d711fa8e92e8c2314d7edc0dc501d0f900
Jayce Azua <jayce.azua@gmail.com>
2020-01-04 14:22:39 -0500

    learning dp

---
+++ practice_facebook/fib_dp.py
@@ -0,0 +1,14 @@
+
+
+def fib_dp(n):
+    cache = [None]*(n+1)
+
+    cache[0] = 0
+    cache[1] = 1
+
+    for i in range(2, n+1):
+        cache[i] = cache[i-1] + cache[i-2]
+    return cache[n]
+
+
+fib_dp(10000)
