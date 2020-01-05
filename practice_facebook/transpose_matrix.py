3cd02a690543d09f37f3a058294d5c5e3160f3ae
Jayce Azua <jayce.azua@gmail.com>
2020-01-05 14:33:34 -0500

    solved

---
+++ practice_facebook/transpose_matrix.py
@@ -0,0 +1,27 @@
+"""
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+Example 1:
+
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+"""
+
+def transpose(matrix):
+  if not matrix:
+    return matrix
+  
+  ans = [[None] * len(matrix) for _ in range(len(matrix[0]))]
+
+  for i, row in enumerate(matrix):
+    for j, val in enumerate(row):
+      ans[j][i] = val
+  return ans
+
