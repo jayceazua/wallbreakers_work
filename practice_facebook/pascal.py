2556601143d977166638646dacbb3d62f57b32a2
Jayce Azua <jayce.azua@gmail.com>
2020-01-06 22:04:42 -0500

    solved

---
+++ practice_facebook/pascal.py
@@ -0,0 +1,39 @@
+"""
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+"""
+
+
+def pascal(num_rows):
+    triangle = []
+
+    for row_num in range(num_rows):
+        # The first and last row elements are always 1.
+        row = [None for _ in range(row_num+1)]
+        row[0], row[-1] = 1, 1
+
+        # Each triangle element is equal to the sum of the elements
+        # above-and-to-the-left and above-and-to-the-right.
+        for j in range(1, len(row)-1):
+            row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+
+        triangle.append(row)
+
+    return triangle
+
+
+print(pascal(5))
