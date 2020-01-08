ba927fd08f17f404ac1cf5e5e1d84606752f45cc
Jayce Azua <jayce.azua@gmail.com>
2020-01-08 16:41:44 -0500

    solved

--- practice_facebook/daily_temp.py
+++ practice_facebook/daily_temp.py
@@ -1,3 +1,4 @@
+from collections import Counter
 """
 Given a list of daily temperatures T, return a list such that, 
 for each day in the input, tells you how many days you would have to wait until a warmer temperature. 
@@ -9,18 +10,43 @@ your output should be [1, 1, 4, 2, 1, 1, 0, 0].
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
 """
 
+
 def dailyTemperatures(T):
-  if not T:
-    return
-  
-  days = [0]*len(T)
-  temps_stack = []
+    if not T:
+        return
 
-  for current_day, current_temp in enumerate(T):
-    while temps and temps_stack[-1][0] < current_temp:
-      _, day = temps_stack.pop()
-      days[day] = current_day - day
-    
-    temps_stack.append((current_temp, current_day))
-  return days
+    days = [0]*len(T)
+    temps_stack = []
 
+    for current_day, current_temp in enumerate(T):
+        while temps and temps_stack[-1][0] < current_temp:
+            _, day = temps_stack.pop()
+            days[day] = current_day - day
+
+        temps_stack.append((current_temp, current_day))
+    return days
+
+
+# ryan mock interview
+
+
+def num_occ(s, k):  # O(n) runtime and O(n) space
+    if not s or not k:
+        return 0
+
+    s_histo = Counter(s)  # O(n) runtime/ space
+    k_histo = Counter(k)
+
+    num = float('inf')
+
+    for char, count in k_histo.items():  # BALLOON
+
+        if char not in s_histo:
+            return 0
+
+        make = s_histo[char]  # 5
+        potential = make // count  # 5
+
+        num = min(num, potential)
+
+    return num
