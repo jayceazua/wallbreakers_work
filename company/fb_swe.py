11959a0e57b8961ec24e332ee43d84698c3fbdd4
Jayce Azua <jayce.azua@gmail.com>
2020-01-08 16:18:51 -0500

    solved

--- company/fb_swe.py
+++ company/fb_swe.py
@@ -15,7 +15,7 @@ Enjoy your interview!
 # 2 1 3 6
 # 5 2 1 3 -> true
 
-# 1 3 6  4
+# 1   3  6  4
 # 2 1 - 1 6
 # 5 2 1  3 -> false
 
@@ -32,36 +32,49 @@ Enjoy your interview!
 ]
 """
 
+
 def isToeplitz(matrix):  # O(n * m)
+    if not matrix:
+        return True
+    rows_length = len(matrix)
 
-    rows = len(matrix)  # n 
-    cols = len(matrix[0])  # m
-
-    for col in range(cols):
-        if not check_diagonal(matrix, 0, col):
-            return False  # O(1)
-
-    for row in range(rows):
-        if not check_diagonal(matrix, row, 0):
-            return False
-
+    for i in range(1, rows_length): # O(n)
+        for j in range(1, matrix[i]): #O(m)
+            # compare to the top left neighbor
+            if matrix[i][j] != matrix[i-1][j-1]:
+                return False
     return True
 
+#     rows = len(matrix)  # n
+#     cols = len(matrix[0])  # m
 
-def check_diagonal(matrix, i, j):  # bool
-    constant = matrix[i][j]
-    i += 1
-    j += 1
-    rows = len(matrix)
-    cols = len(matrix[0])
+#     for col in range(cols - 1):  # check first row
+#         if not check_diagonal(matrix, 0, col):
+#             return False  # O(1)
 
-    while i < rows and j < cols:
-        if matrix[i][j] != constant:
-            return False
-        i += 1
-        j += 1
+#     # we already check the first index of the row's index so start at 1
+#     for row in range(1, rows - 1):
+#         # we do not need to check the last row of the rows because it will be alone it it being single is true in itself.
+#         if not check_diagonal(matrix, row, 0):
+#             return False
 
-    return True
+#     return True
+
+
+# def check_diagonal(matrix, i, j):  # bool
+#     constant = matrix[i][j]
+#     i += 1
+#     j += 1
+#     rows = len(matrix)
+#     cols = len(matrix[0])
+
+#     while i < rows and j < cols:
+#         if matrix[i][j] != constant:
+#             return False
+#         i += 1
+#         j += 1
+
+#     return True
 
 
 # "theweatherisnice" -> "the weather is nice"
@@ -76,21 +89,21 @@ def turn_string_words(s):
     result = []
     word = ""
 
-    for _, char in enumerate(s): # O(n)
+    for char in s:  # O(n)
 
         if _is_in_dictionary(word):
-            result.append(word) # O(1)
-            word = "" # O(1)
-        
-        word += char # O(n)
+            result.append(word)  # O(1)
+            word = ""  # O(1)
+
+        word += char  # O(n)
 
     # last check for the word ...
     if _is_in_dictionary(word):
-      result.append(word)
-      word = ""
+        result.append(word)
+        word = ""
 
-    if word: # if there is still characters
+    if word:  # if there is still characters
         return ""
 
-    sentence = " ".join(result) # O(n)
-    return sentence.strip() # O(n)
+    sentence = " ".join(result)  # O(n)
+    return sentence.strip()  # O(n)
