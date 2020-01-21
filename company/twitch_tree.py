20b84c597ee95a221761863f7a5350e3c4cb7ab9
Jayce Azua <jayce.azua@gmail.com>
2020-01-21 09:37:09 -0800

    solving binary tree problem

---
+++ company/twitch_tree.py
@@ -0,0 +1,17 @@
+r"""
+Reconstruct a binary tree using in-order and pre-order results
+
+Input:
+  in-order -> [10, 30, 40, 50, 60, 70, 90]
+  pre-porder -> [50, 30, 10, 40, 70, 60, 90]
+
+Output:
+
+       50 <- Node Objects
+     /   \
+   30     70
+ /   \   /   \
+10   40 60   90
+
+
+"""
