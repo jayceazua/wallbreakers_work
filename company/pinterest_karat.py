300857460a81b02290f253f92f3d7cc3529bc8e1
Jayce Azua <jayce.azua@gmail.com>
2021-01-04 16:08:47 -0500

    pin coding

---
+++ company/pinterest_karat.py
@@ -0,0 +1,71 @@
+from collections import Counter
+'''
+
+You are running a classroom and suspect that some of your students are passing around the answers to multiple choice questions disguised as random strings.
+
+Your task is to write a function that, given a list of words and a string, finds and returns the word in the list that is scrambled up inside the string, if any exists. There will be at most one matching word. The letters don't need to be contiguous.
+
+Example:
+words = ['cat', 'baby', 'dog', 'bird', 'car', 'ax']
+string1 = 'tcabnihjs'
+find_embedded_word(words, string1) -> cat
+
+string2 = 'tbcanihjs'
+find_embedded_word(words, string2) -> cat
+
+string3 = 'baykkjl'
+find_embedded_word(words, string3) -> None / null
+
+string4 = 'bbabylkkj'
+find_embedded_word(words, string4) -> baby
+
+string5 = 'ccc'
+find_embedded_word(words, string5) -> None / null
+
+string6 = 'breadmaking'
+find_embedded_word(words, string6) -> bird
+
+W = number of words in `words`
+L = maximal string length of each word
+
+'''
+
+
+words = ['cat', 'baby', 'dog', 'bird', 'car', 'ax']
+string1 = "tcabnihjs"
+string2 = "tbcanihjs"
+string3 = "baykkjl"
+string4 = "bbabylkkj"
+string5 = "ccc"
+string6 = "breadmaking"
+
+
+def find_embedded_word(words, string):
+
+  for word in words:
+    # print(word)
+    if found_word(word, string):
+      return word
+
+  return None
+
+
+def found_word(word, string):
+  hist_word = Counter(word)
+  hist_string = Counter(string)
+  hist_word -= hist_string
+  
+  return True if len(hist_word) == 0 else False
+
+  
+  
+
+
+
+if __name__ == "__main__":
+    print(find_embedded_word(words, string1))  # -> cat
+    print(find_embedded_word(words, string2))
+    print(find_embedded_word(words, "baykkjl"))
+    print(find_embedded_word(words, string4))
+    print(find_embedded_word(words, string5))
+    print(find_embedded_word(words, string6))
